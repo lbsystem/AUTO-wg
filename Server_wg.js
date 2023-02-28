60c9f1ef5b2f466c0e71b1033393f60f2c2d9d95
lbsystem <42770927+lbsystem@users.noreply.github.com>
2023-02-28 12:15:30 +0800

    Update Server_wg.js

--- Server_wg.js
+++ Server_wg.js
@@ -1,19 +1,20 @@
 const cp = require("child_process")
 const http = require("http"),
     fs = require("fs"),
-    ini = require('ini')
-
-const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'))
+    ini = require('ini'),
+    path = require("path")
+let filePath = path.join(path.dirname(process.execPath),"config.ini")
+const config = ini.parse(fs.readFileSync(filePath, 'utf-8'))
 console.log(config.ros)
-
 function handle(req,res){
     if(req.method==="GET"){
         let url = new URL("http://192.168.2.35:3000"+req.url)
         let prarment = url.searchParams
         let port = prarment.get("port")
         let name = prarment.get("passwd")
-        let interface  = prarment.get("interface")
+        let interFace  = prarment.get("interface")
         console.log(name);
+        console.log(interFace)
         console.log(config.ros.password);
         if (name !== config.ros.password){
             console.log("not")
@@ -21,14 +22,14 @@ function handle(req,res){
             return;
         }
         try{
-            cp.execSync(`wg set ${interface} listen-port ${port}`)
+            cp.execSync(`wg set ${interFace} listen-port ${port}`)
         }catch(e){
             console.log(e)
         }
-        
+
         // cp.execSync(`wg-quick down ${config.ros.interface}`)
         // cp.execSync(`wg-quick up ${config.ros.interface}`)
-       
+
         // let content = fs.readFileSync("./tset.html")
         console.log(port)
         res.end("ok")
